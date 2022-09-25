055d244d2baddc0a2a28aebc82e15a27fb3f6fee
brother <15926681541@163.com>
2022-09-25 17:33:38 +0800

    1

--- src/main/java/com/example/video/VideoApplication.java
+++ src/main/java/com/example/video/VideoApplication.java
@@ -3,7 +3,7 @@ package com.example.video;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
 public class VideoApplication {
 
     public static void main(String[] args) {
